1b439210316e2fd52bd559393bbac64aa45209d8
Nitin Kamate <nkamate@gmail.com>
2015-09-14 23:24:14 +0530

    Initial Commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('loading');
+  this.route('profile');
+  this.route('blog');
+  this.route('about');
 });
 
 export default Router;
